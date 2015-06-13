fe61c2e43fddbf77ce36768b8011b37e7ce81066
Jarrett Cruger <jcrugzz@gmail.com>
2015-06-13 00:19:07 -0700

    make it so

---
+++ index.js
@@ -0,0 +1,51 @@
+
+var fs = require('fs');
+var once = require('one-time');
+var path = require('path');
+var Forkee = require('forkee');
+var duplexify = require('duplexify');
+var uuid = require('uuid');
+
+module.exports = StrongChild;
+
+function StrongChild() {
+  if (!(this instanceof StrongChild)) return new StrongChild();
+  this.stream = duplexify();
+  this.fork = new Forkee()
+    .on('request', this._onReq.bind(this));
+
+  return this.stream;
+
+}
+
+StrongChild.prototype._onReq = function (message, callback) {
+
+  this.callback = once(callback);
+  this.stream.on('error', this.callback);
+
+  this.filePath = message.__file;
+  this.returnPath = this.return || message.__return || path.join(os.tmpdir(), uuid.v4());
+
+  if (Object.keys(message) > 1) {
+    delete message.__file;
+    this.stream.emit('request', message);
+  }
+
+  //
+  // Set both sides of the Duplex
+  //
+  this.stream.setReadable(fs.createReadStream(this.filePath));
+  this.writable = fs.createWriteStream(this.returnPath)
+    .on('finish', this._onFinish.bind(this));
+
+  this.stream.setWritable(this.writable);
+};
+
+//
+// Return the callback with the path to the file that the parent will use to
+// read from
+//
+StrongChild.prototype._onFinish = function () {
+  this.callback(null, { __file: this.returnPath });
+  this.writable = null;
+};
